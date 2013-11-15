084babede870c8796d820635991fd17dbd510547
rponcio <rpg_2000@latinmail.com>
2013-11-14 21:03:10 -0300

    Avance

---
+++ src/cl/rpgscorp/rpgturnos/AutentificacionActivity.java
@@ -0,0 +1,13 @@
+package cl.rpgscorp.rpgturnos;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AutentificacionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.autentificacion);
+	}
+}
+
